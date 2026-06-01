5d95598ce0b42a2e4f97dfbb028d9643e0f90242
nguyennhuphuongdha-ux <nguyennhuphuongdha@gmail.com>
2026-06-01 18:30:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/safe school ai/',
+    base: '/SAFE SCHOOL AI/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
